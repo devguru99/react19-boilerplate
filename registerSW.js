c228b337a536223d4d4fc85b7480011133806a5e
dmytro <dmytro.pro@outlook.com>
2024-12-29 23:16:37 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/react19-boilerplate/sw.js', { scope: '/react19-boilerplate/' })})}
